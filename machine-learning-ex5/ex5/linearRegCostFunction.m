5e69893af980b33121cb63e73ee53ceaba1a2e1d
Kristian Gennaci <kristiangennaci@Kristians-MBP.home>
2018-01-08 22:01:47 -0500

    adding the finished cost function and gradient code

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,9 +20,23 @@ grad = zeros(size(theta));
 %
 
 
+% Calculating the the Cost Function with Regularization
+h_theta = X * theta;
+error = (1 / (2 * m)) * sum((h_theta - y).^2);
+regularization = (lambda / (2 * m)) * sum(theta(2:end, :).^2);
+J = error + regularization;
 
 
 
+% Calculating Gradient for J=0 (index 1 in MATLAB)
+% grad(1) = ((1 / m) .* (h_theta - y))' * X(:, 1);
+grad(1) = (1 / m) * ((h_theta - y)' * X(:, 1));
+
+% Calculating Gradient for J=1..n (index 2..n+1 in MATLAB)
+%part1 = (((1 / m) .* (h_theta - y))' * X(:, 2:end))';
+part1 = (1 / m) .* ((h_theta - y)' * X(:, 2:end))';
+part2 = (lambda / m) .* theta(2:end);
+grad(2:end) =  part1 + part2;
 
 
 
